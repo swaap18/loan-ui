47abb3fb9b22405261b440efc8b57e3cf88a1879
unknown <ctsjavafsd40@iiht.tech>
2020-06-29 16:07:51 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SearchLoanComponent } from './search-loan/search-loan.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes:Routes=[
+  {path: 'searchloan',component: SearchLoanComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +29,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms'
+import {Validators} from '@angular/forms'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form:FormGroup=new FormGroup({
+  username: new FormControl('',[Validators.required]),
+  password:new FormControl('',[Validators.required]),
+  });
+  hide:boolean=true;
 
-  constructor() { }
+  constructor(public router:Router) { }
+
+  submit(){
+     console.log(this.f.username.value);
+    // console.log(this.form['password'].value);
+    console.log(this.form.value);
+    // this.router.navigate(['searchloan']);
+  }
+  get f(){
+    return this.form.controls;
+  }
+  flagHide(){
+    this.hide=!this.hide;
+  }
 
   ngOnInit(): void {
   }
-
+  getErrorMessage() {
+    if (this.form['username'].hasError('required')) {
+      return 'You must enter a value';
+    }
+}
 }
