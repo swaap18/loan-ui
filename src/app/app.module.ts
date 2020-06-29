d1975bf91688b0020c2aaf32357ec957bb87e8cc
cjaganmohan <cjaganmohan@corelogic.com>
2020-06-29 10:30:08 +0530

    building of search loan UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SearchLoanComponent } from './search-loan/search-loan.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Material Modules
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+
+import {FormsModule} from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +25,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
