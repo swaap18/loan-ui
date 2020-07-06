dd6690c1682dcbfdbf1c96ee5bcaff105fd6c41c
cjaganmohan <cjaganmohan@corelogic.com>
2020-07-06 16:26:17 +0530

    edit loan changes

--- src/app/components/edit-loan/edit-loan.component.ts
+++ src/app/components/edit-loan/edit-loan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-edit-loan',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-loan.component.css']
 })
 export class EditLoanComponent implements OnInit {
+  form: FormGroup;
+  durationInSeconds = 5;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      userFirstName: ['', Validators.required],
+      userLastName: ['', Validators.required],
+      address: this.fb.group({
+        addressLine1: ['', Validators.required],
+        addressLine2: ['', Validators.required],
+        city: ['', Validators.required],
+        zipcode: ['', Validators.required]
+      }),
+      loanAmount: ['', Validators.required],
+      loanTerm: ['', Validators.required],
+      loanId: ['', Validators.required],
+      loanType: ['', Validators.required],
+      legaldocs: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  submit(){
+    
+  }
+
+  disable() {
+    return true;
+  }
+
+  openSnackBar() {
+    this._snackBar.openFromComponent(PizzaPartyComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
+  }
 }
+
+
+@Component({
+  selector: 'snack-bar-component-example-snack',
+  template: '<span class="example-pizza-party"> Successfully Submitted  </span>',
+  styles: [`
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `],
+})
+export class PizzaPartyComponent { }
+
