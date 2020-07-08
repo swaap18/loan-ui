ad2866de69bd1469c0d814f3c45568a79c3e4051
cjaganmohan <cjaganmohan@corelogic.com>
2020-07-08 14:09:31 +0530

    search loan changes

--- src/app/components/search-loan/search-loan.component.ts
+++ src/app/components/search-loan/search-loan.component.ts
@@ -2,9 +2,10 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms'
 import { Validators } from '@angular/forms'
 import { Router } from '@angular/router'
-import { MatSort } from '@angular/material/sort'
-import { MatPaginator } from '@angular/material/paginator'
 import { MatTableDataSource } from '@angular/material/table'
+import { Loan } from '../../shared/model/loan';
+import { LoanService } from 'src/app/services/loan.service';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-search-loan',
@@ -19,27 +20,29 @@ export class SearchLoanComponent implements OnInit {
     loanAmt: new FormControl('', [Validators.minLength(3)])
   });
 
-  displayedColumns: string[] = ['position', 'name', 'loan_num', 'loan_amt'];
-  dataSource = new MatTableDataSource;
+  displayedColumns: string[] = ['id', 'firstName', 'loanId', 'loanAmt'];
+  dataSource = new MatTableDataSource<Loan>();
+  loanList: Loan[] = [];
+  loan: Loan = new Loan();
+  error: string;
 
-  constructor(public router: Router) {
+  constructor(public router: Router, private loanService: LoanService) {
+    // TODO
   }
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit(): void {
-  }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator
-    this.dataSource.sort = this.sort;
   }
 
   submit() {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     console.log("======" + this.form.controls.fullName.value);
-    this.searchLoan(this.form.controls.fullName.value);
+    //this.searchLoan(this.form.controls.fullName.value);
+    this.getLoanByName(this.form.controls.fullName.value);
+    this.loanList = [];
+    this.loanList.push(this.loan);
+    this.dataSource = new MatTableDataSource<Loan>(this.loanList);
+    console.log("loanList:" + this.dataSource.data.values)
+
     //console.log("======FilteredData" + this.dataSource.filteredData);    
   }
 
@@ -47,32 +50,63 @@ export class SearchLoanComponent implements OnInit {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
+    //this.getLoanList();
+
   }
 
-  resetOnClick() {
+  resetOnClick() {    
     this.dataSource = new MatTableDataSource;
+    this.error='';
   }
 
+  getLoanByName(name: string) {
+    this.loanService.getLoanByName(name).subscribe(
+      jsonLoan => {
+        Object.assign(this.loan, JSON.parse(jsonLoan))
+        console.log("After Parse::"+ this.loan.id);
+        console.log("After Parse::"+ this.loan.loanAmount);
+      },
+      err => {
+        console.log("No data found:" + err);
+        this.error = "No data found.";
+      });
+  }
+
+  getLoanList() {
+    this.loanService.getLoans().subscribe(
+      jsonLoanList => {
+        console.log("jsonLoanList" + jsonLoanList);
+        let array: Loan[] = jsonLoanList.loan.forEach(jsonLoan => {
+          var inLoan: Loan = new Loan();
+          return Object.assign(inLoan, JSON.parse(jsonLoan));;
+        });
+
+        this.loanList = array;
+        //Object.assign(this.loanList, JSON.parse(jsonLoanList));
+        this.dataSource = new MatTableDataSource<Loan>(this.loanList);
+        console.log("Data source::" + this.dataSource)
+        console.log("Loan List::" + this.loanList)
+
+      },
+      err => {
+        console.log("No data found:" + err);
+        this.error = `No data found.`
+      });
+
+  }
 }
 
 /* Static data */
-export interface PeriodicElement {
-  position: number;
-  name: string;
-  loan_num: string;
-  loan_amt: number;
+export interface LoanSearchResult {
+  id: number;
+  firstName: string;
+  loanId: string;
+  loanAmt: number;
+  city: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'John', loan_num: '1', loan_amt: 500000.00 },
-  { position: 2, name: 'Smith', loan_num: '2', loan_amt: 500000.00 },
-  { position: 3, name: 'Chris', loan_num: '9898112', loan_amt: 500000.00 },
-  { position: 4, name: 'Joe', loan_num: '9898113', loan_amt: 500000.00 },
-  { position: 5, name: 'Biden', loan_num: '9898114', loan_amt: 500000.00 },
-  { position: 6, name: 'Donald', loan_num: '9898115', loan_amt: 500000.00 },
-  { position: 7, name: 'George', loan_num: '9898116', loan_amt: 500000.00 },
-  { position: 8, name: 'Thamson', loan_num: '9898117', loan_amt: 500000.00 },
-  { position: 9, name: 'Ivanka', loan_num: '9898118', loan_amt: 500000.00 },
-  { position: 10, name: 'Clinton', loan_num: '9898119', loan_amt: 500000.00 },
-  { position: 11, name: 'Tracy', loan_num: '9898119', loan_amt: 500000.00 }
+const SEARCH_RESULT: LoanSearchResult[] = [
+  { id: 1, firstName: 'John', loanId: '1', loanAmt: 500000.00, city: '' },
+  { id: 2, firstName: 'Smith', loanId: '2', loanAmt: 500000.00, city: '' },
+
 ];
--- src/app/services/loan.service.ts
+++ src/app/services/loan.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Loan } from '../shared/model/loan';
 
 const url = 'http://localhost:8088/loanManagementService/';
 const httpOptions = {
@@ -16,6 +19,17 @@ export class LoanService {
    }
    public getLoanById(id:number){
      return this.http.get(url+'loan/'+id);
-     //return this.http.get(url+'loan/search/findByLoanId?loanId='+id);
+   }
+
+   public getLoanByName(name:string): Observable<any>{
+    return this.http.get(url+'loan/search/findByFirstName?firstName='+name,httpOptions).pipe(
+      tap(res => console.log(""),error => console.log('error'))
+      )
+   }
+
+   public getLoans(): Observable<any>{
+    return this.http.get(url+'loan',httpOptions).pipe(
+      tap(res => console.log(""),error => console.log('error'))
+      )
    }
 }
