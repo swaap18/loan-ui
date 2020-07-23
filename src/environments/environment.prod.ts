300ac497fc0093b40681f526636f346bfcdfaa33
swapnil <ctsjavafsd40@iiht.tech>
2020-07-23 17:02:52 +0530

    commits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://loan-service-002.cfapps.io/loanManagementService/',
+  API_URL: 'https://loan-service-002.cfapps.io/loanManagementService/',
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     test: true,
-    API_URL: 'http://loan-manager-api.cfapps.io/loanManagementService/'
+    API_URL: 'https://loan-service-002.cfapps.io/loanManagementService/'
   };
   
